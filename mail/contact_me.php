981cc8c1b2abdeab84acdd2fa24720a2c762e918
nader <nader.al.aghbari1@gmail.com>
2020-03-27 14:33:07 +0100

    Portfolio recuperer information formulaire

---
+++ mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// Check for empty fields
+if(empty($_POST['Nom'])  		||
+   empty($_POST['Adresse email']) 		||
+   empty($_POST['Numéro de téléphone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$Nom = strip_tags(htmlspecialchars($_POST['name']));
+$Adresse email= strip_tags(htmlspecialchars($_POST['Adresse email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+	
+// Create the email and send the message
+$to = 'nader.al.aghbari1@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
